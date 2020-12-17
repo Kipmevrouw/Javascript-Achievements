6c196ce22cda2213dd63c8b7e1a535b1ef165325
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2020-12-17 10:28:15 +0100

    peanut

---
+++ Quiz/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Peanut",
+        "imageURI": "https://vignette.wikia.nocookie.net/spongebobsquarepants/images/1/1e/SpongeBob_Bikinibroek_s_nachts.png/revision/latest/top-crop/width/360/height/450?cb=20200622130302&path-prefix=nl"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat is de naam krekel in het alfabet",
+				"answers": [{
+						"answer": "Peanut",
+						"feedback": true
+					},
+					{
+						"answer": "Aardappel",
+						"feedback": true
+					},
+					{
+						"answer": "Appelmoes",
+						"feedback": false
+					},
+					{
+						"answer": "Moesappel",
+						"feedback": false
+					},
+					{
+						"answer": "Sinaasappel",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Hoe spel je peanut",
+				"answers": [{
+						"answer": "Pienten",
+						"feedback": false
+					},
+					{
+						"answer": "Peanut",
+						"feedback": true
+					},
+					{
+						"answer": "Paetnut",
+						"feedback": false
+					},
+					{
+						"answer": "Peatpeat",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Hoe oud is Peanut",
+				"answers": [{
+						"answer": "8",
+						"feedback": false
+					},
+					{
+						"answer": "9",
+						"feedback": false
+					},
+					{
+						"answer": "12",
+						"feedback": false
+					},
+					{
+						"answer": "2",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
---
+++ Quiz/quiz2.js
@@ -0,0 +1,65 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "Peanut",
+        "imageURI": "https://vignette.wikia.nocookie.net/spongebobsquarepants/images/1/1e/SpongeBob_Bikinibroek_s_nachts.png/revision/latest/top-crop/width/360/height/450?cb=20200622130302&path-prefix=nl"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat is peanut + peanut ",
+				"answers": [{
+						"answer": "9",
+						"feedback": false
+					},
+					{
+						"answer": "10",
+						"feedback": false
+					},
+					{
+						"answer": "11",
+						"feedback": true
+					},
+					{
+						"answer": "13",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is 22",
+				"answers": [{
+						"answer": "12",
+						"feedback": false
+					},
+					{
+						"answer": "22",
+						"feedback": true
+					},
+					{
+						"answer": "32",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Houd peanut van peanut",
+				"answers": [{
+						"answer": "Nee",
+						"feedback": false
+					},
+					{
+						"answer": "soms",
+						"feedback": false
+					},
+					{
+						"answer": "jaa",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
